19c87d286edaab8c27375c170bda558aab1946d5
Brad <walker.brad26@gmail.com>
2023-06-16 00:56:07 -0500

    testing, adjustments

---
+++ src/components/Silver.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Silver.scss';
+
+export default function silver() {
+  return (
+    <div className='silverMainDiv'>
+      silver
+        <h1>Big Silver Monies. test</h1>
+
+
+    </div>
+  )
+}
+
